5904f35b4b684996294c37b27ab982809906a09e
c212 <40395648+c212@users.noreply.github.com>
2024-04-06 23:38:32 -0400

    Create Node.java

---
+++ project/insert/Node.java
@@ -0,0 +1,24 @@
+public class Node {
+  int key;
+  int color; // 0 is Red, 1 is Black and so on.
+  static final int RED = 0, BLACK = 1;
+  Node left, right;
+  public Node(int key, int color) {
+    this(key, null, null, color);
+  }
+  public Node(int key) {
+    this(key, null, null, BLACK);
+  }
+  public Node(int key, Node left, Node right, int color) {
+      this.key = key;
+      this.left = left;
+      this.right = right;
+      this.color = color;
+  }
+  public Node(int key, Node left, Node right) {
+    this(key, left, right, BLACK);
+  }
+  public void accept(Visitor v) {
+      v.visitNode(this);
+  }
+}
