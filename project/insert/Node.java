b0db1edc15273c5768dcaad4ea86e5e8e4f58271
c212 <40395648+c212@users.noreply.github.com>
2024-04-07 00:01:06 -0400

    Adding basic insert to Node.java

--- project/insert/Node.java
+++ project/insert/Node.java
@@ -21,4 +21,21 @@ public class Node {
   public void accept(Visitor v) {
       v.visitNode(this);
   }
+  public void insert(int key) {
+    // assume the key is not in there (externally check with find first)
+    if (key < this.key)
+      if (this.left != null)
+        this.left.insert(key);
+      else this.left = new Node(key, Node.RED);
+    else
+      if (this.right != null)
+        this.right.insert(key);
+      else this.right = new Node(key, Node.RED);
+  }
+  public boolean find(int key) {
+    if (key == this.key) return true;
+    else if (key < this.key && this.left != null) return this.left.find(key);
+    else if (key > this.key && this.right != null) return this.right.find(key);
+    else return false;
+  }
 }
