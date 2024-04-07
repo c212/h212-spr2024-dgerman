205b93a6a508133cf5f7bce6e877147ccd115cc8
c212 <40395648+c212@users.noreply.github.com>
2024-04-07 00:02:13 -0400

    Testing basic insert in Node with Driver.java

--- project/insert/Driver.java
+++ project/insert/Driver.java
@@ -1,23 +1,55 @@
 public class Driver {
     public static void main(String[] args) {
-        Node a = new Node(2, new Node(1, Node.RED), new Node(3, Node.RED));
-        a.accept(new Reporter()); System.out.println("---------------");
-        Node b = new Node(2, new Node(1, Node.RED), new Node(3, null, new Node(5, Node.RED), Node.RED), Node.RED);
-        b.accept(new Reporter()); System.out.println("---------------");
+        int[] values = new int[] { 4, 2, 3, 1, 5, 6 };
+        Node a = null;
+        for (int i : values) {
+          System.out.println("Adding: " + i);
+          if (a == null)
+            a = new Node(i);
+          else
+            a.insert(i);
+          a.accept(new Reporter()); System.out.println("---------------");
+        }
     }
 }
 /*******(sample run:
 
 dgerman@silo:/l/www/classes/h212/spr2024/project$ java Driver
-  _2_
- /   \
-[1] [3]
+Adding: 4
+4
 ---------------
+Adding: 2
+  _4
+ /
+[2]
+---------------
+Adding: 3
+  ____4
+ /
+[2]_
+    \
+   [3]
+---------------
+Adding: 1
+     ____4
+    /
   _[2]_
  /     \
-[1]   [3]_
-          \
-         [5]
+[1]   [3]
+---------------
+Adding: 5
+     ____4_
+    /      \
+  _[2]_   [5]
+ /     \
+[1]   [3]
+---------------
+Adding: 6
+     ____4_
+    /      \
+  _[2]_   [5]_
+ /     \      \
+[1]   [3]    [6]
 ---------------
 dgerman@silo:/l/www/classes/h212/spr2024/project$
 
