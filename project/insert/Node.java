da9fa2d179fc947a6b231ab0d64a30376778c57d
c212 <40395648+c212@users.noreply.github.com>
2024-04-07 22:53:07 -0400

    Associated Node.java for the first case of double red.

--- project/insert/Node.java
+++ project/insert/Node.java
@@ -10,16 +10,30 @@ public class Node {
     this(key, null, null, BLACK);
   }
   public Node(int key, Node left, Node right, int color) {
-      this.key = key;
-      this.left = left;
-      this.right = right;
-      this.color = color;
+    this.key = key;
+    this.left = left;
+    this.right = right;
+    this.color = color;
   }
   public Node(int key, Node left, Node right) {
     this(key, left, right, BLACK);
   }
   public void accept(Visitor v) {
-      v.visitNode(this);
+    v.visitNode(this);
+  }
+  public static Node insertInto(Node node, int key) {
+    if (node == null) return new Node(key, BLACK);
+    else {
+      node.insert(key);
+      node.accept(new Reporter());
+      while (doubleRed(node)) {
+        node = fixIt(node);
+        node.color = BLACK;
+        (new java.util.Scanner(System.in)).nextLine();
+        node.accept(new Reporter());
+      }
+      return node;
+    }
   }
   public void insert(int key) {
     // assume the key is not in there (externally check with find first)
@@ -31,6 +45,26 @@ public class Node {
       if (this.right != null)
         this.right.insert(key);
       else this.right = new Node(key, Node.RED);
+  }
+  public static boolean doubleRed(Node node) {
+    if (node.color == RED && (node.left != null && node.left.color == RED || node.right != null && node.right.color == RED)) return true;
+    else return node.left != null && doubleRed(node.left) || node.right != null && doubleRed(node.right);
+  }
+  public static Node fixIt(Node n) {
+    System.out.print("Fixing it... ");
+    if (n.color == BLACK && n.left != null && n.left.color == RED && n.left.right != null && n.left.right.color == RED) {
+      // System.out.println("I detected it...");
+      // System.exit(0);
+      Node n1 = n.left, n2 = n.left.right, n3 = n, t1 = n1.left, t2 = n2.left, t3 = n2.right, t4 = n3.right;
+      n1.color = BLACK;
+      n1.right = t2;
+      n3.left = t3;
+      n2.left = n1;
+      n2.right = n3;
+      return n2;
+    } else {
+      return n;
+    }
   }
   public boolean find(int key) {
     if (key == this.key) return true;
