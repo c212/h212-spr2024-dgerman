db5c920c774740c05b688b6473f6fa33d000b2ce
c212 <40395648+c212@users.noreply.github.com>
2024-04-08 00:52:29 -0400

    The final Driver.java class for Stage 1.

--- project/insert/Driver.java
+++ project/insert/Driver.java
@@ -1,17 +1,38 @@
 public class Driver {
     public static void main(String[] args) {
-        System.out.println("Inserting " + 3 + " into the empty tree...");
-        Node n = Node.insertInto(null, 3);
+        System.out.println("Inserting " + 1 + " into the empty tree...");
+        Node n = Node.insertInto(null, 1);
         n.accept(new Reporter());
-        System.out.println("-------------");
-        System.out.println("Adding " + 1);
-        n = Node.insertInto(n, 1);
-        // n.accept(new Reporter());
         System.out.println("-------------");
         System.out.println("Adding " + 2);
         n = Node.insertInto(n, 2);
         // n.accept(new Reporter());
         System.out.println("-------------");
+        System.out.println("Adding " + 3);
+        n = Node.insertInto(n, 3);
+        // n.accept(new Reporter());
+        System.out.println("-------------");
+        System.out.println("Adding " + 4);
+        n = Node.insertInto(n, 4);
+        // n.accept(new Reporter());
+        System.out.println("-------------");
+        System.out.println("Adding " + 5);
+        n = Node.insertInto(n, 5);
+        // n.accept(new Reporter());
+        System.out.println("-------------");
+        System.out.println("Adding " + 6);
+        n = Node.insertInto(n, 6);
+        // n.accept(new Reporter());
+        System.out.println("-------------");
+        System.out.println("Adding " + 7);
+        n = Node.insertInto(n, 7);
+        // n.accept(new Reporter());
+        System.out.println("-------------");
+        System.out.println("Adding " + 8);
+        n = Node.insertInto(n, 8);
+        // n.accept(new Reporter());
+        System.out.println("-------------");
+
     }
 }
 
@@ -19,25 +40,91 @@ public class Driver {
 
 dgerman@silo:/l/www/classes/h212/spr2024/project$ javac Driver.java
 dgerman@silo:/l/www/classes/h212/spr2024/project$ java Driver
-Inserting 3 into the empty tree...
-3
--------------
-Adding 1
-  _3
- /
-[1]
+Inserting 1 into the empty tree...
+1
 -------------
 Adding 2
-  ____3
- /
-[1]_
-    \
-   [2]
+1_
+  \
+ [2]
+-------------
+Adding 3
+1_
+  \
+ [2]_
+     \
+    [3]
 Fixing it...
  2
 / \
 1 3
 -------------
-dgerman@silo:/l/www/classes/h212/spr2024/project$ 
+Adding 4
+ 2
+/ \
+1 3_
+    \
+   [4]
+-------------
+Adding 5
+ 2
+/ \
+1 3_
+    \
+   [4]_
+       \
+      [5]
+Fixing it...
+ 2__
+/   \
+1  [4]
+  /   \
+  3   5
+-------------
+Adding 6
+ 2__
+/   \
+1  [4]
+  /   \
+  3   5_
+        \
+       [6]
+-------------
+Adding 7
+ 2__
+/   \
+1  [4]
+  /   \
+  3   5
+        \
+       [6]_
+           \
+          [7]
+Fixing it...
+ 2__
+/   \
+1  [4]__
+  /     \
+  3    [6]
+      /   \
+      5   7
+Fixing it...
+  _4_
+ /   \
+ 2   6
+/ \ / \
+1 3 5 7
+-------------
+Adding 8
+  _4_
+ /   \
+ 2   6
+/ \ / \
+1 3 5 7_
+        \
+       [8]
+-------------
+dgerman@silo:/l/www/classes/h212/spr2024/project$
+
 
  ****(end of sample run)***/
